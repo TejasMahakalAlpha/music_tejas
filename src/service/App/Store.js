c4819c9def565f3fe2449c1f1a1a9af9a2840ecc
Tejas Mahakal <tejasmahakal@gtasterix.com>
2025-10-28 12:21:24 +0530

    solve deployment issue

--- src/service/App/Store.js
+++ src/service/App/Store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from '../features/auth/authSlice';
-
+// import userReducer from '../features/auth/authSlice';
+import userReducer from '../Features/auth/authSlice';
 export const store = configureStore({
   reducer: {
     user: userReducer,
